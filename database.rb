2ab9791073b3f4228fcd45f430ab0bee30549f3c
Hamilton Clark <dhclark2@ncsu.edu>
2016-11-16 18:31:25 -0500

    Instructor add a search loop with the index object.

--- database.rb
+++ database.rb
@@ -6,106 +6,108 @@
 
 class Person
   attr_accessor :name, :position, :phone_number, :address, :salary, :github, :slack
-end
 
-@people = []
+  @people = []
 
-  def no_match
-    puts "That person is not in our records."
-  end
+#  def no_match
 
-  def incorrect_entry
-    puts "You entered an incorrect option."
-  end
+#  end
 
-  def add_entry
-    person = Person.new
+#  def incorrect_entry
+    #puts "You entered an incorrect option."
+#  end
 
-    puts "What is their name?"
-    person.name = gets.chomp
-    puts "What is their phone number, (###)-###-####"
-    person.phone_number = gets.chomp
-    puts "What is their address?"
-    person.address = gets.chomp
-    puts "What is their position at The Ironyard?"
-    person.position = gets.chomp
-    puts "What is their salary?"
-    person.salary = gets.chomp
-    puts "What is their Slack username?"
-    person.slack = gets.chomp
-    puts "What is their GitHub username?"
-    person.github = gets.chomp
+#  def add_entry
 
-    @people << person
-  end
+#  end
 
-  def search_entry
-    puts "What is the first name of the person you want to search for?"
-    name = gets.chomp
+#  def search_entry
 
-    if @people.include? name
-      puts
-      puts "#{name}: "
-      puts
-      puts "Phone #:  #{phone}"
-      puts "Address:  #{address}"
-      puts "Position held by #{name} at The Iron Yard: #{position}"
-      puts "Salary ($): #{salary}"
-      puts "Slack Account: #{slack}"
-      puts "GitHub Account: #{github}"
+  #end
 
-    else
-      no_match
+#  def delete_entry
 
-    end
-  end
-
-  def delete_entry
-    puts "What is the first name of the person you want to delete?"
-      name = gets.chomp
-
-    if @people.include? "name"
-      person.delete("name")
-
-    else
-      puts "That person is not in our records."
-
-    end
-  end
-
-  def end_program_routine
-    puts "Would you like to make another search? (y/n)"
-      search_again = gets.chomp
-
-    if search_again == 'n'
-      puts "Thank you. Goodbye."
-
-    else
-
-    end
-  end
-
-
-
+#  end
 
   loop do
-
+    puts
     puts "Hello.  Type 'A' to add a person, 'S' to search for an entry, 'D' to delete."
       choice = gets.chomp
 
     if choice == "A"
-      add_entry
+      person = Person.new
+
+      puts "What is their name?"
+      person.name = gets.chomp
+      puts "What is their phone number, (###)-###-####"
+      person.phone_number = gets.chomp
+      puts "What is their address?"
+      person.address = gets.chomp
+      puts "What is their position at The Ironyard?"
+      person.position = gets.chomp
+      puts "What is their salary?"
+      person.salary = gets.chomp
+      puts "What is their Slack username?"
+      person.slack = gets.chomp
+      puts "What is their GitHub username?"
+      person.github = gets.chomp
+
+      @people << person
 
     elsif choice == "S"
-      search_entry
+      #  search_entry
+      puts "What is the first name of the person you want to search for?"
+      name = gets.chomp
+
+      index = 0
+      loop do
+        if index >= @people.length
+          puts "That person is not in our records."
+          break
+        end
+
+        person = @people[index]
+        if name == person.name
+          puts
+          puts "#{name}: "
+          puts
+          puts "Phone #:  #{phone}"
+          puts "Address:  #{address}"
+          puts "Position held by #{name} at The Iron Yard: #{position}"
+          puts "Salary ($): #{salary}"
+          puts "Slack Account: #{slack}"
+          puts "GitHub Account: #{github}"
+        end
+
+        index = index + 1
+      end
 
     elsif choice == "D"
-      delete_entry
+        #  delete_entry
+      puts "What is the first name of the person you want to delete?"
+      name = gets.chomp
 
-    else incorrect_entry
+      if @people.include?(name)
+        @people.delete(name)
 
+      else
+      puts "That person is not in our records."
+
+      end
     end
 
-  end_program_routine
-
   end
+
+  # end_program_routine
+
+  puts
+  puts "Would you like to make another search? (y/n)"
+    search_again = gets.chomp
+
+  if search_again == 'n'
+    puts "Thank you. Goodbye."
+  else
+    return
+  end
+
+end
